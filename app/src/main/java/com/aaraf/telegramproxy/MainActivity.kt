d7a3160577158d057a1ae15fa2b6ed6346406044
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-11-15 07:22:45 +0500

    InitialCHeck

--- app/src/main/java/com/aaraf/telegramproxy/MainActivity.kt
+++ app/src/main/java/com/aaraf/telegramproxy/MainActivity.kt
@@ -2,25 +2,41 @@ package com.aaraf.telegramproxy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import com.aaraf.telegramproxy.models.GetProxyList
 import com.aaraf.telegramproxy.utilities.APIService
 import com.aaraf.telegramproxy.utilities.ServiceBuilder
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    var token = "testtoken"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        getProxyList(token)
 
-        val responseVar = ServiceBuilder.getInstance()
-            .create(APIService::class.java)
+    }
 
-        GlobalScope.launch {
-            val response = responseVar.getProxiesList("testtoken")
-            if (response != null)
-                Log.d("RESPONSE", response.toString())
-        }
+    private fun getProxyList(s: String) {
+        val response = ServiceBuilder.getInstance(APIService::class.java)
+        response.getProxiesList(s).enqueue(
+            object : Callback<GetProxyList> {
+                override fun onResponse(
+                    call: Call<GetProxyList>,
+                    response: Response<GetProxyList>
+                ) {
+                    Toast.makeText(this@MainActivity, response.body().toString(), Toast.LENGTH_LONG)
+                        .show()
+                }
+
+                override fun onFailure(call: Call<GetProxyList>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, t.toString(), Toast.LENGTH_LONG).show()
+                }
+
+            }
+        )
     }
 }
--- app/src/main/java/com/aaraf/telegramproxy/utilities/ServiceBuilder.kt
+++ app/src/main/java/com/aaraf/telegramproxy/utilities/ServiceBuilder.kt
@@ -7,11 +7,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
 
-    val BASEURL = "https://ir.muzhifei.com/v1/"
+    val BASEURL = "https://ir.muzhifei.com/"
 
-    fun getInstance(): Retrofit {
-        return Retrofit.Builder().baseUrl(BASEURL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASEURL) // change this IP for testing by your actual machine IP
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <T> getInstance(service: Class<T>): T {
+        return retrofit.create(service)
     }
 }
